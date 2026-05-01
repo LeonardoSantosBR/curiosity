6ca513f4582b81be97653652fd02b4f4a28d26e6
Leonardo Santos <leosantosmelo07@gmail.com>
2026-04-30 23:15:23 -0300

    refactor: restructure layout by moving Header and SafeAreaView to RootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,14 @@
-import '@/global.css';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Stack } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { Header } from "@/components";
+import "@/global.css";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const queryClient = new QueryClient();
 
@@ -11,8 +17,11 @@ export default function RootLayout(): React.JSX.Element {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack screenOptions={{ headerShown: false }} />
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <SafeAreaView className="flex-1">
+          <Header />
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaView>
       </ThemeProvider>
     </QueryClientProvider>
   );
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,5 @@
-import { Header } from "@/components";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Potd } from "./Potd";
 
 export default function HomeScreen(): React.JSX.Element {
-  return (
-    <SafeAreaView className="flex-1">
-      <Header />
-      <Potd />
-    </SafeAreaView>
-  );
+  return <Potd />;
 }
